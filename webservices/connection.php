1cc66c277b3031879cc7219c92eb5c23d37c5bb4
vadimbaele <vadim@novomine.com>
2015-02-02 13:12:14 +0100

    3

--- webservices/connection.php
+++
@@ -1,3 +0,0 @@
-<?php $username="helios";$password="heliosnovominef0VVrhUa";$database="heliosdata";$con=mysql_connect('localhost',$username,$password);@mysql_select_db($database) or die( "Unable to select database");
-
-?>
